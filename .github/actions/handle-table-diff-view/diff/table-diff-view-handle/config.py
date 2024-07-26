1c665f31f24487f31db1f35633129c7566c4a970
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2024-07-26 12:23:57 +0200

    Add diff table aciton

---
+++ .github/actions/handle-table-diff-view/diff/table-diff-view-handle/config.py
@@ -0,0 +1,24 @@
+import os
+
+# Configuration constants.
+class Configuration:
+    env_file = os.getenv('GITHUB_ENV')
+    # Table header count
+    TABLE_HEADER_COUNT = 5
+    # Highlight color for new data
+    NEW_DATA_COLOR = "red"
+    # Highlight color for old data
+    OLD_DATA_COLOR = "gold"
+    # Github repository owner
+    GITHUB_REPO_OWNER = "truongquangsb"
+    # Github repository name
+    GITHUB_REPO_NAME = "set"
+    # Github Token
+    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+    # Github api url
+    GITHUB_API_URL = "https://api.github.com/repos"
+    # Diff markdown header
+    DIFF_MD_HEADER = "# Table difference view:"
+
+
+CONFIG = Configuration()
