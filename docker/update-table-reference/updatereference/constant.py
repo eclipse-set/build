a956db4627ffc1548729dca8b39fb18b02409553
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2024-09-10 18:00:43 +0200

    Add update table reference image

---
+++ docker/update-table-reference/updatereference/constant.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Constanst:
+    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+    GITHUB_API_URL = "https://api.github.com/repos"
+
+    SET_REPO = "eclipse-set/set"
+    TABLE_REFERENCE_ARTIFACT_NAME_PATTERN = "table-csv-{}"
+    BUILD_SET_WORK_FLOW_ID = 63604658
+    SET_TABLE_REFERENCE_PATH = (
+        "java/bundles/org.eclipse.set.swtbot/test_res/table_reference"
+    )
+
+
+CONSTANT = Constanst()
